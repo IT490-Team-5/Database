11f95bdb2fb7274d5746cc7682fc03a4acb1b7fa
Duttp1998 <50345085+Duttp1998@users.noreply.github.com>
2021-03-30 17:31:34 -0400

    Update senddb.py

--- senddb.py
+++ senddb.py
@@ -1,6 +1,7 @@
 import pika
 import mysql.connector
 import json
+import sys
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -10,32 +11,47 @@ mydb = mysql.connector.connect(
 )
 def callback(ch, method, properties, body):
     info = json.loads(body)
-    mycursor = mydb.cursor()
-    mycursor.execute(info)
-    result= mycursor.fetchall()
-    
-    d1={}
-    for i in results:
-        a = i[0]
-        b = i[1]
-        d[a] = b
-    
+    q = info.get("create") or info.get("login")
+    v = info.get("values")
+    print(type(v))
+    print(info)
+    mycursor = mydb.cursor() # Create a cursor object
+    results = None
+    try:
+        mycursor.execute(q, v) # Execute a query
+        results = mycursor.fetchall() # Fetch all results from our query (a row). Return None on failure.
+    except mysql.connector.errors.IntegrityError:
+        print("Tried to insert row which already exists!")
+
     d2={}
-    d2["query"] = d1
-    d2["reason"] = "results"
+    d2["reason"] = "results"    
+
+    if results is not None:
+        d2["query2"] = "success"
+        mydb.commit()
+    else:
+        d2["query2"] = "failure"
+
 
     r=json.dumps(d2)
-    channel.basic_publish(exchange='', routing_key='hello' , body=r)
-    print("[x] Sent 'Hello World!'")
+    channel.basic_publish(exchange='', routing_key='hello', body=r)
+    print("Received! Relaying results to middle..")
     
+try:
+    credentials = pika.PlainCredentials('admin', 'admin')
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='25.121.196.54',port=5672,virtual_host = "vh1", credentials=credentials))
+    channel = connection.channel()
 
-credentials = pika.PlainCredentials('admin', 'admin')
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='25.121.196.54',port=5672, credentials=credentials))
-    
-channel = connection.channel()
+    channel.queue_declare(queue='database')
+    channel.basic_consume('database', callback, auto_ack=True)
+    print("Listening on queue: Database")
+    channel.start_consuming()
 
-channel.queue_declare(queue='database')
-channel.basic_consume('database', callback, auto_ack=True)
-print("Listening on queue: Database")
-channel.start_consuming()
+except KeyboardInterrupt:
+    mydb.close()
+    sys.exit(0)
+finally:
+    try:
+        mydb.close()
+    except:
+        pass
